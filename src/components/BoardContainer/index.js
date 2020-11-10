228a357c4a7263ea00717d9a53f3e5b2e7c2996d
Dana Pham <danapham12@gmail.com>
2020-11-09 20:00:38 -0600

    finished setting up firebase

---
+++ src/components/BoardContainer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class BoardContainer extends Component {
+  state = { }
+
+  render() {
+    return (
+      <div className='BoardContainer'>
+        <h2>Boards</h2>
+      </div>
+    );
+  }
+}
