d12f80457fc60634fba9a417d4cf4051cd74a3eb
Dana Pham <danapham12@gmail.com>
2020-11-23 19:12:01 -0600

    boards can be deleted now

--- src/views/Boards.js
+++ src/views/Boards.js
@@ -14,6 +14,10 @@ export default class Boards extends React.Component {
     this.getBoards();
   }
 
+  componentDidUpdate() {
+    this.getBoards();
+  }
+
   getBoards = () => {
     const currentUserId = getUid();
     boardsData.getAllBoards(currentUserId).then((res) => this.setState({
@@ -21,13 +25,19 @@ export default class Boards extends React.Component {
     }));
   }
 
+  deleteBoard = (e) => {
+    const fbKey = e.target.id;
+    boardsData.deleteBoard(fbKey);
+    this.getBoards();
+  };
+
   render() {
     const { boards } = this.state;
     return (
       <>
       <h1>My Boards</h1>
       <AppModal title={'Create Board'} buttonLabel={'Create Board'}><BoardForm onUpdate={this.getBoards} /></AppModal>
-      {boards.map((board) => <Board key={board.firebaseKey} board={board} />)}
+      {boards.map((board) => <Board key={board.firebaseKey} board={board} deleteBoard={this.deleteBoard} />)}
       </>
     );
   }
